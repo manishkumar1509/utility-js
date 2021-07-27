2b9b2ad6ade31270708fa73750dcf48ac650264c
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2021-07-27 13:38:09 +0530

    Added head testcases and code

---
+++ src/head.js
@@ -0,0 +1,6 @@
+const head = (list) => {
+    if (list.length==0) return null;
+    return list[0];   
+}
+
+module.exports = head;
---
+++ src/head.spec.js
@@ -0,0 +1,12 @@
+const head = require('./head');
+
+describe('Head', () => {
+
+    it('head([1,2,3]) should be 1', () => {
+        expect(head([1,2,3])).toEqual(1);
+    });
+
+    it('head([]) should be null or undefined', () => {
+        expect(head([])).toEqual(null);
+    });
+})
